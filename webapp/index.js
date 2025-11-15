2d51b6b1270ac2cef5b4256ac2afdf45ec0c5ae1
Augustinkalisa <unrwabatt12@gmail.com>
2025-11-15 12:22:20 +0200

    Update index.js
    good

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,17 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
   if (req.url === '/about') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('About Page\n');
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
   }
 });
+
 server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/'>
+  console.log('Server running at http://localhost:3000/');
 });
