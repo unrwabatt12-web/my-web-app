84ca35f5dbe5535954c39b922bfa67d60feb4871
KALISA <unrwabatt12@gmail.com>
2025-11-15 09:17:00 +0200

    Add /about route to web application

---
+++ webapp/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/about') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About Page\n');
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+  }
+});
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
